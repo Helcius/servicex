1426106d9d519940e459e99d982aa7b91919ce8d
Helcius Araújo Nóbrega Cabral <114032954+Helcius@users.noreply.github.com>
2024-03-11 20:02:25 -0300

    Delete servicex directory

--- servicex/src/main/java/br/com/helciuscabral/servicex/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.helciuscabral.servicex.repository;
-
-import br.com.helciuscabral.servicex.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
