83cbccd06cd70a0a487762539d6c7471e96da833
helcius <helcius.cabral@hotmail.com>
2024-03-11 21:27:40 -0300

    feat: Delete mapping e Put mapping

--- src/main/java/br/com/helciuscabral/servicex/resources/CategoriaResource.java
+++ src/main/java/br/com/helciuscabral/servicex/resources/CategoriaResource.java
@@ -36,4 +36,23 @@ public class CategoriaResource {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
 
     }
+
+    @DeleteMapping("/{idCategoria}")
+    public ResponseEntity<Void> deletarCategoria(@PathVariable Integer idCategoria){
+        categoriaService.deletarCategoria(idCategoria);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{idCategoria}")
+    public ResponseEntity<Categoria> atualizarCategoria(@PathVariable Integer idCategoria, @RequestBody Categoria categoria){
+        if(!categoriaService.buscarCategoria(idCategoria).isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        categoria.setIdCategoria(idCategoria);
+        Categoria novaCategoria = categoriaService.atualizarCategoria(categoria);
+        return new ResponseEntity<>(novaCategoria, HttpStatus.OK);
+    }
+
+
+
 }
